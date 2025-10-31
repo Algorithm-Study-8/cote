8a749c3b410f8b5db875751fea542a8d24958651
PUPAO <tkdldjstm0127@gmail.com>
2025-10-31 15:41:29 +0900

    백준 코드

---
+++ Hakjun/cote/src/BOJ1541.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * 수식을 입력했다. 괄호를 임의로 추가하여, 가장 작은 수를 만들어라.
+ * 덧셈을 완료한 뒤, 뺄셈을 계산하자
+ */
+public class BOJ1541 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+
+        String[] numbers = input.split("[+-]");
+        int[] numberList = Arrays.stream(numbers).mapToInt(Integer::parseInt).toArray();
+
+        List<Character> operations = new ArrayList<>();
+        for (char c : input.toCharArray()) {
+            if (c != '+' && c != '-') {
+                continue;
+            }
+
+            operations.add(c);
+        }
+
+        for (int i = operations.size() - 1; i >= 0; i--) {
+            if (operations.get(i) == '+') {
+                numberList[i] += numberList[i + 1];
+                numberList[i + 1] = 0;
+            }
+        }
+
+        int result = numberList[0];
+        for (int i = 0; i < operations.size(); i++) {
+            if (operations.get(i) == '-') {
+                result -= numberList[i + 1];
+            }
+        }
+        System.out.println(result);
+    }
+}
